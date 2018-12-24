30b99c2e5dbe4a2325e61f960f3e3020fa378926
Sumuga Padman <sumugapadman@yahoo.in>
2018-12-24 16:21:15 +0530

    Modified urlEncode and utf-8 encode

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+/***
+ * Author : Sumuga Padman
+ * App : Simple - Rest - APi
+ */
+
+/* Require -- START -- */
 const express = require("express");
 const path = require('path');
+const utf8 = require('utf8');
+var urlencode = require('urlencode');
 var bodyParser = require('body-parser');
+/* Require -- END -- */
+
+
 const app = new express();
 const PORT = process.env.PORT || 3000;
 
@@ -20,7 +31,8 @@ app.get('/:statusCode',function(req,res){
     }else if (req.params.statusCode == 2002){
         res.append('Cache-Control','must-revalidate,no-cache,no-store');
         res.append('Content-Type','text/html; charset=UTF-8');
-        res.statusMessage = 'Erreur interne du serveur avec des caractères multi-octets (à,é)';
+        //res.statusMessage = utf8.encode("Erreur Interne du Serveur à,é");
+        res.statusMessage = urlencode("Erreur Interne du Serveur à,é");
         return res.status(200).sendFile(path.join(__dirname+'/views/2002.html'));
     } else {
         return res.send(req.params);
@@ -31,12 +43,13 @@ app.get('/:statusCode',function(req,res){
 app.post('/v1', function (req, res) {
     console.log(req.body);
     if(req.body.statusCode == 500){
-        res.statusMessage = 'Internal Server Error without multibyte characters';
         res.append('Cache-Control','must-revalidate,no-cache,no-store');
         res.append('Content-Type','text/html; charset=UTF-8');
         return res.status(500).sendFile(path.join(__dirname+'/views/500.html'));
     }else if (req.body.statusCode == 2002){
-        res.statusMessage = 'Erreur interne du serveur avec des caractères multi-octets (à,é)';
+        //res.statusMessage = utf8.encode("Erreur Interne du Serveur à,é");
+        res.statusMessage = urlencode("Erreur Interne du Serveur à,é");
+        console.log(res.statusMessage);
         res.append('Cache-Control','must-revalidate,no-cache,no-store');
         res.append('Content-Type','text/html; charset=UTF-8');
         return res.status(500).sendFile(path.join(__dirname+'/views/2002.html'));
